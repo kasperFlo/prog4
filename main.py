1c4240d514d18675f2ff11a3c502ac58ed731cd8
aleksbrsc <aleksbrsc@gmail.com>
2022-12-03 02:42:21 -0500

    fixed Book class variable names

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 class Book:
-    def __init__(self, name, author, yearPublished):
-        self.name = name
-        self.author = author
-        self.yearPublished = yearPublished
+    def __init__(self, _name, _author, _yearPublished):
+        self._name = _name
+        self._author = _author
+        self._yearPublished = _yearPublished
 
     def getName(self):
-        return self.name
+        return self._name
+
+    def setName(self, x):
+        self._name = x
     
     def getAuthor(self):
-        return self.author
+        return self._author
+    
+    def setAuthor(self, x):
+        self._author = x
     
     def getYearPublished(self):
-        return self.yearPublished
-
-
+        return self._yearPublished
+    
+    def setYearPublished(self, x):
+        self._yearPublished = x
